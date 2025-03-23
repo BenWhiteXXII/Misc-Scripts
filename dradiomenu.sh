b7cef197154180aa3f457414ebed75e268d014c5
BenWhiteXXII <75984510+BenWhiteXXII@users.noreply.github.com>
2025-03-23 15:28:34 +0000

    Updated due to BBC Radio stream changes

--- dradiomenu.sh
+++ dradiomenu.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 # Dmenu script to grab bbc radio live streams
-
-radioSta=$(printf "bbc_radio_one\\nbbc_radio_two\\nbbc_radio_three\\nbbc_radio_fourfm\\nbbc_radio_fourlw\\nbbc_radio_five_live\\nbbc_1xtra\\nbbc_radio_one_dance\\nbbc_radio_one_relax\\nbbc_radio_four_extra\\nbbc_radio_five_live_sports_extra\\nbbc_6music\\nbbc_asian_network\\nbbc_world_service" | dmenu -i -p "Select Station:")
-mpv --force-window --keep-open=yes "http://as-hls-uk-live.akamaized.net/pool_904/live/uk/$radioSta/$radioSta.isml/$radioSta-audio%3d320000.norewind.m3u8"
+radioSta=$(printf "BBC\\nClassicFM" | dmenu -i -p "Select Station:")
+if [ "$radioSta" = 'BBC' ]
+then
+	bbcSta=$(printf "bbc_radio_one\\nbbc_radio_two\\nbbc_radio_three\\nbbc_radio_fourfm\\nbbc_radio_fourlw\\nbbc_radio_five_live\\nbbc_1xtra\\nbbc_radio_one_dance\\nbbc_radio_one_relax\\nbbc_radio_four_extra\\nbbc_radio_five_live_sports_extra\\nbbc_6music\\nbbc_asian_network\\nbbc_world_service" | dmenu -i -p "Select BBC Station:")
+	#url="http://as-hls-ww-live.akamaized.net/pool_01505109/live/ww/$bbcSta/$bbcSta.isml/$bbcSta-audio%3d48000.norewind.m3u8"
+	url="http://a.files.bbci.co.uk/ms6/live/3441A116-B12E-4D2F-ACA8-C1984642FA4B/audio/simulcast/dash/nonuk/pc_hd_abr_v2/ak/$bbcSta.mpd"
+elif [ "$radioSta" = 'ClassicFM' ]
+then
+	url="https://www.globalplayer.com/live/classicfm/uk/"
+fi
+mpv --force-window --keep-open=yes $url
